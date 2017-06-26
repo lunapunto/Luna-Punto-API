2f447210a5876a9bc330b187cbb7601d304e7110
Mauricio Robles <mauricio@lunapunto.com>
2017-06-26 14:11:58 -0500

    killerAPI.php file added

---
+++ api/killerAPI.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+** KILLER API
+** Utilizado para detener remotamente páginas en proceso.
+**
+**/
+
+
+
+/*
+* () checkclientkc
+* Revisa los valores de killclient.
+* @uses class killLP
+* @params $_REQUEST - clientid varchar (obligatorio)
+* @returns (obj) {msg (string), error (bool), kill (bool), clientid (int) }
+*/
+function checkclientkc(){
+  $client = v('clientid', $_REQUEST);
+  $kill = new killLP($client);
+  return $kill->result();
+}
+/*
+* () closeclient
+* Devuelve un booleano determinando si el cliente está en el blacklist.
+* @parent checkclientkc()
+* @params $_REQUEST - clientid varchar (obligatorio)
+* @returns (bool) ¿el cliente está en el blacklist? true : false
+*/
+function closeclient(){
+  $killresult = checkclientkc();
+  $iskill = $killresult->kill;
+  echo $iskill;
+  die();
+}
